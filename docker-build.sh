0ca51a3a843fb907acbbfb704bee21b286139de6
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-09-02 12:17:41 +0200

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -3,4 +3,5 @@
 [ ! -d "./chdb" ] && git clone https://github.com/chdb-io/chdb
 [ ! -d "./ccache" ] && mkdir ccache
 
+docker build -t chdb-builder .
 docker run --rm -ti -v $(pwd)/chdb:/chdb -v $(pwd)/ccache:/ccache chdb-builder
