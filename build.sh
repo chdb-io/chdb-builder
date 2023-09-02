4a8daef11c8ad503459e2ed0a62db1c930a12919
root <lorenzo.mangani@gmail.com>
2023-09-02 14:26:39 +0000

    resync

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 [ ! -d "./chdb" ] && git clone https://github.com/chdb-io/chdb
 [ ! -d "./ccache" ] && mkdir ccache
+[ ! -d "./dist" ] && mkdir dist
 
 docker build -t chdb-builder .
-docker run --rm -ti -v $(pwd)/chdb:/chdb -v $(pwd)/ccache:/ccache chdb-builder
+docker run --rm -ti -v $(pwd)/chdb:/chdb -v $(pwd)/ccache:/ccache -v $(pwd)/dist:/local chdb-builder
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,13 @@ cd /chdb && git pull && git submodule update --init --recursive --jobs 4
 echo "Building chdb... "
 cd /chdb && bash ./chdb/build.sh
 
+echo "Building wheel... "
+python3 -m pip install -U pybind11 wheel build tox
+python3 -m build --wheel
+
+[ -d "/local" ] && cp ./dist/*.whl /local/
+
+echo "Build Ready!"
+ls -alF dist
+
 exec "$@"
