d4575e47dcb56118ff8188f4eb661b1c8533af12
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-09-04 13:06:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Locate chdb sources... "
 [ ! -d "/chdb" ] && git clone https://github.com/chdb-io/chdb /chdb
 
 echo "Updating submodules... "
-cd /chdb && git pull && git submodule update --init --recursive --jobs 0
+cd /chdb && git pull && git submodule update --init --recursive --jobs 4
 
 echo "Building chdb... "
 cd /chdb && bash ./chdb/build.sh
